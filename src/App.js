f4eac800664ba2b5b525e8599be81a3112398179
Pete Hall <pete.mack.hall@gmail.com>
2023-03-18 14:51:28 -0500

    Added template function and first error component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Error from './components/Error/Error';
 
 function App() {
   return (
     <div className="App">
       <h1>The start</h1>
+      <Error />
     </div>
   );
 }
---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import {useSelector} from 'react-redux';
+
+function Error() {
+
+  // const store = useSelector((store) => store);
+  // const [heading, setHeading] = useState('Functional Component');
+
+  return (
+    <div>
+      <h2>Error component</h2>
+    </div>
+  );
+  
+}
+
+export default Error;
