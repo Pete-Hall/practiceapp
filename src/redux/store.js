2458a045fc53adb0c262d7f91d2d36522c3472fa
Pete Hall <pete.mack.hall@gmail.com>
2023-03-19 15:26:38 -0500

    reconfigure store to accept apiSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./apiSlice";
 import errorSlice from "./errorSlice";
 
 export const store = configureStore({
   reducer: {
-    errors: errorSlice
+    errors: errorSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(apiSlice.middleware)
 });
