1fcd143f09e39df01e4c32975f9b823a6d320e2b
Pete Hall <pete.mack.hall@gmail.com>
2023-03-19 15:22:34 -0500

    Create apiSlice

---
+++ src/redux/apiSlice.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// replaces axios
+// interact with api
+
+// Define our single API slice object
+export const apiSlice = createApi({
+  // The cache reducer expects to be added at `state.api` (already default - this is optional)
+  reducerPath: 'api',
+  // All of our requests will have URLs starting with '/fakeApi'
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5001' }),
+  // The "endpoints" represent operations and requests for this server
+  endpoints: builder => ({
+    // The `getPosts` endpoint is a "query" operation that returns data
+    getErrors: builder.query({
+      // The URL for the request is '/fakeApi/posts'
+      query: () => '/api/getErrors'
+    })
+  })
+});
+
+// Export the auto-generated hook for the `getPosts` query endpoint
+export const { useGetErrorsQuery } = apiSlice;
