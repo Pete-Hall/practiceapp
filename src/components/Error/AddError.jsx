327e04dde17ccba50d9b0067afa132e32b42b493
Pete Hall <pete.mack.hall@gmail.com>
2023-04-04 15:35:49 -0500

    add hook to capture user input data for adding a new error

--- src/components/Error/AddError.jsx
+++ src/components/Error/AddError.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const AddError = () => {
+
+  const [newError, setNewError] = useState('');
+
+  useEffect(() => {
+    console.log(newError);
+  }, [newError]);
   
   return(
     <div>
-      <input type="text" />
+      <input type="text" onChange={(e) => setNewError(e.target.value)}/>
     </div>
   )
   
