c378ac05cf6f6ce4c3b8f4abcca48c3444b0b8e5
Pete Hall <pete.mack.hall@gmail.com>
2023-04-04 15:14:10 -0500

    Update GET route to pull from Database

--- server/routers/getErrors.router.js
+++ server/routers/getErrors.router.js
@@ -2,29 +2,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-const errors = [
-  {
-    id: "1",
-    description: "Test 1 description",
-    flag: "True",
-    comments: "Test 1 comments"
-  },
-  {
-    id: "2",
-    description: "Test 2 description",
-    flag: "False",
-    comments: "Test 2 comments"
-  },
-];
-
 router.get('/', (req, res) => {
   // https://stackoverflow.com/questions/58450951/blocked-by-cors-policy-error-when-calling-to-mongo-golang-db-with-angular-web-ap/58451189#58451189
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
-  console.log("hello from getErrors.router");
-  res.send(errors);
+  let queryString = `SELECT * FROM rtkquery ORDER BY id ASC;`
+  pool.query(queryString).then((results) => {
+    console.log("hello from getErrors.router", results.rows);
+    res.send(results.rows);
+  }).catch((err) => {
+    console.log(err);
+    res.sendStatus(500);
+  })
 });
 
 module.exports = router;
