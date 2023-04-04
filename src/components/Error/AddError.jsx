12bd892c7753f53eae3ef441f29577aee25027a0
Pete Hall <pete.mack.hall@gmail.com>
2023-04-04 14:51:53 -0500

    Created AddError file

---
+++ src/components/Error/AddError.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const AddError = () => {
+  
+  return(
+    <div>
+      <input type="text" />
+    </div>
+  )
+  
+}
--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { SingleErrorContent } from './SingleError';
 import { AllErrorContent } from './AllErrors';
 import { SingleErrorMultipleParamsContent } from './SingleErrorMultipleParams';
+import { AddError } from './AddError';
 
 export const ErrorList = () => {
 
@@ -13,6 +14,8 @@ export const ErrorList = () => {
       <SingleErrorContent />
       <h2>Single Error Multiple Query Params from RTK Query</h2>
       <SingleErrorMultipleParamsContent />
+      <h2>Add an error (not realistic but curious how to do this)</h2>
+      <AddError />
     </section>
   )
 }
