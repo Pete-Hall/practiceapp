3767002a6955bd5aa76ffc52e7b6406b0e7b1542
Pete Hall <pete.mack.hall@gmail.com>
2023-04-04 15:05:32 -0500

    Created database table, updated pg pool config

---
+++ database.sql
@@ -0,0 +1,11 @@
+-- Create table in database
+CREATE TABLE "rtkquery" (
+    "id" SERIAL PRIMARY KEY,
+    "description" VARCHAR (1000),
+    "flag" BOOLEAN,
+    "comments" VARCHAR (1000)
+);
+
+-- Insert some test records into the table
+INSERT INTO rtkquery ("description", "flag", "comments") VALUES ('Test 1 description', 'True', 'Test 1 comments');
+INSERT INTO rtkquery ("description", "flag", "comments") VALUES ('Test 2 description', 'False', 'Test 2 comments');
