c96391a40a9dda4e24d491e1c91b6a0ee4498a68
Pete Hall <pete.mack.hall@gmail.com>
2023-04-06 12:41:37 -0500

    Updated AddError with mutation to send description

--- src/components/Error/AddError.jsx
+++ src/components/Error/AddError.jsx
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import { useAddNewErrorMutation } from '../../redux/apiSlice';
 
 export const AddError = () => {
+
+  const [addNewError, { isLoading }] = useAddNewErrorMutation();
 
   const [newError, setNewError] = useState('');
 
   useEffect(() => {
     console.log(newError);
   }, [newError]);
+
+  const canSave =[newError].every(Boolean) && !isLoading;
+
+  const onSave = async() => {
+    if(canSave) {
+      try {
+        await addNewError({newError}).unwrap();
+        setNewError('');
+      } catch(err) {
+        console.error('Failed to save the post: ', err)
+      }
+    }
+  }
   
   return(
     <div>
       <input type="text" placeholder="Enter description" onChange={(e) => setNewError(e.target.value)}/>
+      <button onClick={onSave}>Save</button>
     </div>
   )
   
